9c66a823124c351f59049eb39e021eabb8fcf0e1
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2026-04-08 11:18:25 +0900

    Refactor to use DFS for graph traversal
    Refactored the code to implement a DFS approach instead of Dijkstra's algorithm for solving the problem. Updated input handling and output formatting.

--- sunjong/graph/26-04-08.py
+++ sunjong/graph/26-04-08.py
@@ -1,35 +1,44 @@
-from heapq import heappop, heappush
+# https://www.acmicpc.net/problem/11403
 
+import sys
 
-def dijkstra(start_node):
-    pq = [[start_node, 0]]
-    dists = [int(21e8)] * (n + 1)
-    dists[start_node] = 0
+def solve():
+    # 빠른 입력을 위해 sys.stdin.readline 사용
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    
+    n = int(input_data[0])
+    graph = []
+    curr_idx = 1
+    for i in range(n):
+        graph.append(list(map(int, input_data[curr_idx:curr_idx + n])))
+        curr_idx += n
 
-    while pq:
-        node, dist = heappop(pq)
+    def dfs(start, target):
+        stack = [start]
+        vis = [0] * n
+        # Java 코드와 마찬가지로 vis[start] = 1을 미리 하지 않음 (사이클 확인용)
 
-        for nxt_node, nxt_dist in graph[node]:
-            new_dist = dist + nxt_dist
+        while stack:
+            cur_node = stack.pop()
 
-            if dists[nxt_node] <= new_dist:
-                continue
+            for next_node in range(n):
+                # 연결되어 있고 방문하지 않았다면
+                if graph[cur_node][next_node] == 1 and vis[next_node] == 0:
+                    if next_node == target:
+                        return 1
+                    
+                    vis[next_node] = 1
+                    stack.append(next_node)
+        return 0
 
-            dists[nxt_node] = new_dist
-            heappush(pq, [nxt_node, new_dist])
-    return dists
+    # 결과 출력
+    for i in range(n):
+        row_result = []
+        for j in range(n):
+            row_result.append(str(dfs(i, j)))
+        print(" ".join(row_result))
 
-
-tc = int(input())
-
-for t in range(1, tc + 1):
-    n, e = map(int, input().split())
-    graph = [[] for _ in range(n + 1)]
-
-    for _ in range(e):
-        s, e, w = map(int, input().split())
-        graph[s].append([e, w])
-
-    dists = dijkstra(0)
-
-    print(f'#{t} {dists[n]}')
+if __name__ == "__main__":
+    solve()
